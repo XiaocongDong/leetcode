a7751df10f41007e5231ee03c932fa6e28cfd9a3
xiaocdon <xiaocdon@cisco.com>
2017-02-08 22:00:30 -0800

    Add explanation to h-index problem

--- h-index.py
+++ h-index.py
@@ -1,3 +1,12 @@
+"""
+Explanation: when the array is sorted, you can make sure after one specific index, the rest of the number
+are all larger than one number, let say we have an index i, from index i, there are N - i numbers which 
+are bigger than citations[i], if citations[i] >= N - i, it means there are (N - i) citations is larger than 
+N - i, and if i is the first index that meets this criteria, we can also draw the conclusion that 
+the former numbers of the array, are all smaller than N - i, this assumption can be proved as follow:
+	citations[i] >= N - i  ==> i is the first one which match  ==>
+	citations[i - 1] < N - i + 1 ==> citations[i - 1] <= N - i  
+"""
 class Solution(object):
     def hIndex(self, citations):
         """
