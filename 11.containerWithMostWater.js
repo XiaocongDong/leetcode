84b0a50af1fb0e0f24c4739d57ad6bcd8ee2bfcc
Xiaocong Dong <15652914852@163.com>
2017-07-26 17:04:20 -0700

    added solution for 11

---
+++ 11.containerWithMostWater.js
@@ -0,0 +1,32 @@
+// question link https://leetcode.com/problems/container-with-most-water/#/description
+
+/**
+ * My solution, pointers collison
+ * 
+ */
+
+ /**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let i = 0, j = height.length - 1;
+    let max = 0;
+    
+    while(i < j) {
+        let volume = (j - i) * Math.min(height[i], height[j]);
+        
+        max = Math.max(volume, max);
+        if(height[i] < height[j]) {
+            i++;
+        }
+        else if(height[i] > height[j]) {
+            j--;
+        }
+        else {
+            j--;
+        }
+    }
+    
+    return max;
+};
